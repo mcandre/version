353e6f1d200ee4a8b7f6158d8d4afe76cddccf69
Andy Pennebaker <andy.pennebaker@wicourts.gov>
2014-07-10 16:59:38 -0500

    erlang

---
+++ erlang/version.erl
@@ -0,0 +1,15 @@
+%% Compile
+%%
+%% erlc -Wall version.erl
+%%
+%% Run
+%%
+%% erl -s version -noshell
+
+-module(version).
+-author("andrew.pennebaker@gmail.com").
+-export([start/0]).
+
+start() ->
+  io:format("~s~n", [erlang:system_info(otp_release)]),
+  init:stop().
