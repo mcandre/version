44fb4aa3ae698c3dbbd2e1f5b13c17e56bed983c
Andy Pennebaker <andy.pennebaker@wicourts.gov>
2014-07-31 09:04:01 -0500

    module

--- lua/version.lua
+++ lua/version.lua
@@ -1,3 +1,13 @@
 #!/usr/bin/env lua
 
-print(_VERSION)
+local main
+main = function()
+  print(_VERSION)
+end
+
+if type(package.loaded[(...)]) ~= "userdata" then
+  main(arg)
+else
+  module(..., package.seeall)
+end
+
