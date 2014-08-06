cc704e458a178914ae62831ed239aa70fddd5273
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2014-08-06 16:57:28 -0500

    d

---
+++ d/version.d
@@ -0,0 +1,13 @@
+#!/usr/bin/env rdmd
+
+import std.stdio;
+import std.string;
+import std.compiler;
+
+void main(const string[] args) {
+  string v = format("%d", __VERSION__);
+  string c = name;
+
+  writeln("Version: ", v);
+  writeln("Compiler: ", c);
+}
