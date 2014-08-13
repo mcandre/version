1d8b31f33e96a51d1361e4b0f573eeaa730d27c8
Andy Pennebaker <andy.pennebaker@wicourts.gov>
2014-08-13 15:59:10 -0500

    linux

--- objc/Version.m
+++ objc/Version.m
@@ -1,7 +1,11 @@
 #import <Foundation/Foundation.h>
 
 int main() {
+  NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+
   NSLog(@"%s\n", __VERSION__);
+
+  [pool drain];
 
   return 0;
 }
