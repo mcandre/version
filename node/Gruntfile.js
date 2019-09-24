cdcc25d7374c67182eab56d61e702d1bbcd88b7b
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2019-09-24 17:04:36 -0500

    jsfmt

--- node/Gruntfile.js
+++ node/Gruntfile.js
@@ -2,6 +2,7 @@ module.exports = function(grunt) {
     grunt.initConfig({
         exec: {
             test: 'npm test',
+            jsfmt: 'find . -type d -name node_modules -prune -o -type f -name "*[.-]min.js" -prune -o \\( -type f -name "*.js" -o -type f -path "*/bin/*" \\) -exec node node_modules/.bin/jsfmt -w {} \\;',
             jshint: 'node node_modules/jshint/bin/jshint .',
             eslint: 'node node_modules/eslint/bin/eslint .',
         }
@@ -10,8 +11,12 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-exec');
 
     grunt.registerTask('default', ['exec:test']);
+
     grunt.registerTask('test', ['exec:test']);
-    grunt.registerTask('lint', ['exec:jshint', 'exec:eslint']);
+
+    grunt.registerTask('lint', ['exec:jsfmt', 'exec:jshint', 'exec:eslint']);
+
+    grunt.registerTask('jsfmt', ['exec:jsfmt']);
     grunt.registerTask('jshint', ['exec:jshint']);
     grunt.registerTask('eslint', ['exec:eslint']);
 };
